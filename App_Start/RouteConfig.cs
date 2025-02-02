ac80ec8d277fd38e6a64221c11420ae7a52111a4
Sumit Timande <sumittimandetimande@gmail.com>
2025-02-02 08:19:10 +0530

    changes on rought.config

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NimapTask
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "DisplayProducts", id = UrlParameter.Optional }
             );
         }
     }
